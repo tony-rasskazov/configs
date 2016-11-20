7de2965a161c65d10c93d7d1810a640da83cacc5
Tony Rasskazov <tony.rasskazov@gmail.com>
2016-11-20 21:04:42 +0300

    dev

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
+# todo INSTALL script
+
 cfgs=~/Workspace/configs/bash
 
 #cp $cfgs/bash_profile ~/.bash_profile
-
-ditto $cfgs/inputrc ~/.inputrc
-
-ditto $cfgs/gitconfig ~/.gitconfig
+#ditto $cfgs/inputrc ~/.inputrc
+#ditto $cfgs/gitconfig ~/.gitconfig
 
 
 
